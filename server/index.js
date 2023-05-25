1d411e563c3231cd196d14ff6112c34de29285d0
aloksingh1st <aloksingh1st@gmail.com>
2023-05-25 22:29:03 +0530

    web rtc video chat

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const emailToSocketIdMap = new Map();
 const socketIdToEmail = new Map();
 
 io.on("connection", (socket)=>{
+
     console.log("Socket connection established", socket.id);
     socket.on("room:join", data=>{
         const {email, roomId} = data;
@@ -23,7 +24,25 @@ io.on("connection", (socket)=>{
         io.to(socket.id).emit("room:join", data)
     })
 
-    socket.on("user:call", ({to , offer})=>{
-        io.to(to).emit("incoming:call",{from:socket.id, offer})
-    })
-})
+    socket.on("user:call", data => {
+        const {to, offer} = data;
+
+        // console.log("User:call ", to);
+        io.to(to).emit("incomming:call", { from: socket.id, offer });
+      });
+
+    socket.on("call:accepted", ({to, ans})=>{
+        io.to(to).emit("accepted", {from: socket.id, ans});
+    });
+
+    socket.on("peer:nego:needed", ({to , offer})=>{
+        io.to(to).emit("peer:nego:needed", {from: socket.id, offer});
+    });
+
+    socket.on("peer:nego:done", ({ to, ans }) => {
+        // console.log("peer:nego:done", ans);
+        io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+      });
+
+
+});
